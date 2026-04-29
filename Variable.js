9111f6103c032f7c6bdfc11674f177d920366d90
ramyar66630@gmail.com <ramyar66630@gmail.com>
2026-04-29 22:09:41 +0530

    file added

---
+++ Variable.js
@@ -0,0 +1,25 @@
+//Variable declaration
+let bookName,sum,diff;
+//Variable Initialization
+bookName = "Harry potter";
+let bookName$="hps";
+let book_Name="Love"
+
+let bookAuthor = "J.K. Rowling",
+     bookPublisher = "Code io",
+     bookPart = "Prisoner of Askabar";
+
+//we can change the value of the variable
+bookName="HP";
+//we can store the value from one variable to another
+bookName=bookAuthor;
+
+console.log(bookName,bookAuthor,"\n",bookPart);
+
+/* Rules to name a Variable
+    1.-unique variable should declared only unique should not repeat
+    2.$_
+    3.can have digits/should not start with digits
+    4.keywords are not allowed
+    5.case senstive (capital letter and small letter cosider as different ex bookName /BookName both are different)
+*/
