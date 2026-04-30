df3f09fd65d9fdd5ed6f4ca924019cadf0d5f23e
ramyar66630@gmail.com <ramyar66630@gmail.com>
2026-04-30 11:41:20 +0530

    file added

---
+++ constant.js
@@ -0,0 +1,15 @@
+const x = sum(2,3);//not a hot coded so ndentifier be in small leter
+
+const UNAME = "ramya66630@gmal.com";
+const PWD  = "thisIsMyPass";//hot code ha eruintha value run pandrathuku munadiyae therinja adha hot code apo adhoda iedntifier caps la dha erukano  
+
+const WHITE_COLOUR = "#fff";
+const BLACK_COLOUR = "#000";
+
+login(UNAME,PWD);
+
+let whiteColour;//cammel case
+const DATE_OF_BIRTH = 11/8/2005;
+const age = calAge(DATE_OF_BIRTH);
+
+
